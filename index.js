930db1c32537eff1c877d140176a9b07b7b34bdf
Aziz Yokubjonov <yakubjanov0@hotmail.com>
2017-12-20 23:23:05 +0000

    adding & deleting

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 /*global React*/
 /*global ReactDOM*/
 
-//Part One: Implement the TODO App from here: https://goo.gl/vySbPk
 //Part Two: Add a delete button to each list element.
 
 function List(props) {
     //TODO: Create list from props.
     return (<ul>
+    {
+      props.items.map(function(x, i){return <li key={i}>{x} <button onClick={() => {props.delete(i)}}>Delete</button></li>})
+    }
     </ul>);
 };
 
@@ -14,14 +16,29 @@ var App = React.createClass({
   getInitialState: function() {
     //TODO: Set the initial state.
     return {
-        
+        current:'',
+        items:[]
     };
   },
   onChange: function(event) {
-    //TODO: Save current value in input field.
+    this.setState({...this.state, current:event.target.value});
   },
   onSubmit: function(event) {
-      //TODO: Add to list.
+    event.nativeEvent.preventDefault();
+    let items = [...this.state.items, this.state.current];
+    this.setState({
+      current:'',
+      items:[...items]
+    });
+    console.log(this.state);
+  },
+  onDelete: function(index){
+    this.setState({
+      ...this.state,
+      items:[
+        ...this.state.items.filter((x, i) => { return i != index })
+      ]
+    });
   },
   render: function() {
     //TODO: Add event handlers to input and button.
@@ -29,10 +46,10 @@ var App = React.createClass({
       <div>
         <h2>Todo List</h2>
         <form className="App">
-          <input/>
-          <button>Add</button>
+          <input onChange={this.onChange} value={this.state.current}/>
+          <button onClick={this.onSubmit}>Add</button>
         </form>
-        <List items={this.state.items} />
+        <List items={this.state.items} delete={this.onDelete}/>
       </div>
     );
   }
